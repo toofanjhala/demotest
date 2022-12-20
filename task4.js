86c91387fb54e11f228948c3b6ebbfe58db543eb
toofanjhala <121038143+toofanjhala@users.noreply.github.com>
2022-12-20 17:29:37 +0530

    task4

---
+++ task4.js
@@ -0,0 +1,21 @@
+//console.log(document.all);
+//document.all[10].textContent="hello"
+//let doc=document.querySelectorAll("main-header");
+ headertitle=document.getElementById("main-header")
+//headertitle.textContent="hello world"
+
+headertitle.style.borderBottom="solid 5px #000";
+newitem=document.getElementsByClassName("title");
+//console.log(newitem)
+newitem[0].style.color="green"
+newitem[0].style.fontWeight="bold"
+
+let items =document.querySelectorAll(".list-group")
+items[0].style.border="solid 2px red"
+
+let listman =document.querySelectorAll(".list-group-item")
+listman[0].style.fontWeight = "bold";
+listman[1].style.fontWeight= "bold";
+listman[2].style.fontWeight= "bold";
+listman[2].style.backgroundColor = "green";
+listman[3].style.fontWeight = "bold";
